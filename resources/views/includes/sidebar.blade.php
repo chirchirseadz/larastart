a58123f2245eaa67fc5942a5afa5123181a4774a
Sydney kipchirchir <sydney.kipchirchir@BIMASKENYA.local>
2024-12-30 14:48:28 +0300

    Compleated with the ui set up

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,70 +1,66 @@
-<aside class="app-sidebar bg-primary-subtle shadow" data-bs-theme="dark">
+<aside class="app-sidebar bg-secondary-subtle shadow" data-bs-theme="dark">
 
     <div class="sidebar-brand">
-
-        <a href="./index.html" class="brand-link">
-
-            <img style="margin-left: -2rem;" src="{{ asset('img/logo.png') }}" alt="lara start Logo"
-                class="brand-image opacity-100 shadow pr-2" />
-
-            <span class="brand-text ">LARA START</span>
+        <a href="#" class="brand-link">
+            <img src="{{ asset('img/logo.png') }}" alt="AdminLTE Logo" class="brand-image opacity-100 shadow" />
+            <span class="brand-text fw-light">Lara start and vue js</span>
         </a>
-
+    
     </div>
-
-
-    <div class="image d-flex mt-3">
-
-        <img style="margin-left: 1.5rem;" width="20%" src="{{ asset('img/profile.png') }}"
-            class="img-circle elevation-2 pl-3" alt="User Image">
-        <a style="text-decoration: none; color:white; margin-top: 0.5rem; margin-left: 1rem;" href="#"
-            class="d-block fs-5 ">Alexander Pierce</a>
-    </div>
-
-
 
 
     <div class="sidebar-wrapper">
         <nav class="mt-2">
-
+        
             <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
-                <li class="nav-item menu-open">
-                    <a href="#" class="nav-link active">
-                        <i class="nav-icon bi bi-speedometer"></i>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-palette"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fa fa-cog"></i>
                         <p>
-                            Dashboard
+                            Management
                             <i class="nav-arrow bi bi-chevron-right"></i>
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="./index.html" class="nav-link active">
+                            <a href="./widgets/small-box.html" class="nav-link">
                                 <i class="nav-icon bi bi-circle"></i>
-                                <p>Dashboard v1</p>
+                                <p>Small Box</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="./index2.html" class="nav-link">
+                            <a href="./widgets/info-box.html" class="nav-link">
                                 <i class="nav-icon bi bi-circle"></i>
-                                <p>Dashboard v2</p>
+                                <p>info Box</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="./index3.html" class="nav-link">
+                            <a href="./widgets/cards.html" class="nav-link">
                                 <i class="nav-icon bi bi-circle"></i>
-                                <p>Dashboard v3</p>
+                                <p>Cards</p>
                             </a>
                         </li>
                     </ul>
                 </li>
+
+
                 <li class="nav-item">
-                    <a href="./generate/theme.html" class="nav-link">
-                        <i class="nav-icon bi bi-palette"></i>
-                        <p>Theme Generate</p>
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fa fa-power-off"></i>
+                        <p>Logout </p>
                     </a>
                 </li>
-            </ul>
 
+
+            </ul>
+        
         </nav>
     </div>
 
